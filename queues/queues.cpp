d7a2bf0847bd344b110c4d036ae355f86c594bf4
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-31 11:06:10 +0700

    try

--- queues/queues.cpp
+++ queues/queues.cpp
@@ -88,7 +88,14 @@ int main() {
 
 	while (true) {
 		try {
-
+			cout << "Menu" << endl;
+			cout << " 1.Implement insert operation" << endl;
+			cout << " 2.Implement delete operation" << endl;
+			cout << " 3.Display values" << endl;
+			cout << " 4. Exit" << endl;
+			cout << "Enter your choice (1-4):  ";
+			cin >> ch;
+			cout << endl;
 		}
 	}
 }
